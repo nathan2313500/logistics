7f2f1fddad6163600ae9076d6059939a9551ca24
nathan2313500 <76025369+nathan2313500@users.noreply.github.com>
2021-04-27 10:45:39 +0800

    1

---
+++ files/原型图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r],s,_(t,u,v,w,g,x,y,_(),z,[],A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,M,N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,_(G,H,I,Z),ba,R,bb,bc,_(bd,be,bf,bg,bh,bg,bi,bg,I,_(bj,bk,bl,bk,bm,bk,bn,bo)),i,_(j,k,l,k)),bp,_(),bq,_(),br,_(bs,[])),bt,_(),bu,_());}; 
+var b="url",c="原型图.html",d="generationDate",e=new Date(1619491508872.8),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="currentTime",s="page",t="packageId",u="a66ac45f8eda4f748496c9df2b9fcba2",v="type",w="Axure:Page",x="原型图",y="notes",z="annotations",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageAlignment",M="near",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="borderFill",Z=0xFF797979,ba="cornerRadius",bb="cornerVisibility",bc="outerShadow",bd="on",be=false,bf="offsetX",bg=5,bh="offsetY",bi="blurRadius",bj="r",bk=0,bl="g",bm="b",bn="a",bo=0.349019607843137,bp="adaptiveStyles",bq="interactionMap",br="diagram",bs="objects",bt="masters",bu="objectPaths";
+return _creator();
+})());
